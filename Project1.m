ad7685f155d730d7784244f7215acd9531f4e5ca
Josh Tozer <joshiantozer@gmail.com>
2019-09-20 10:10:30 -0600

    Added matlab file

---
+++ Project1.m
@@ -0,0 +1,10 @@
+data = dlmread('Amatrix');
+
+rows = data(1);
+col = data(2);
+fprintf('data: ')
+disp(data);
+fprintf('rows: ');
+fprintf('%d\n', rows);
+fprintf('columns: ')
+fprintf('%d\n', col);
