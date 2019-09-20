233c59be519b183ad3887a5ebfab8603cf0b6364
Josh Tozer <joshiantozer@gmail.com>
2019-09-20 10:43:45 -0600

    Added prototype for loop

--- Project1.m
+++ Project1.m
@@ -1,10 +1,19 @@
 data = dlmread('Amatrix');
 
-rows = data(1);
+row = data(1);
 col = data(2);
 fprintf('data: ')
 disp(data);
 fprintf('rows: ');
-fprintf('%d\n', rows);
+fprintf('%d\n', row);
 fprintf('columns: ')
 fprintf('%d\n', col);
+
+matrixA = zeros(rows, col);
+
+for c = 1:col
+    for r = 1:row
+        matrixA(r, c) = data(3);
+    end
+end
+disp(matrixA);
